222aebe112f1ced60133a174f1b0ead9492f2bc4
varholl <matias.laino@gmail.com>
2021-09-15 00:30:30 -0300

    Set prod smtp

---
+++ app/models/grupa_csv.rb
@@ -0,0 +1,29 @@
+require 'csv'
+
+class GrupaCsv
+	def self.to_csv(data)
+		headers = ['Nombre', 'Provincia']
+		(2012..2020).each do |year|
+			['Integrantes', 'Usuarios Mudados?', 'Cantidad Mudados', 'Mudados Siguen', 'Mudados Municipio', 'Base Grupa', 'Otras Zonas'].each do |sub_header|
+				headers << "#{year} #{sub_header}"
+			end
+		end
+		csv_file = ::CSV.generate(col_sep:"|") do |csv|
+			csv << headers
+			row =[data[:nombre], data[:provincia]]
+			data[:years].each do |year|
+				
+				row << year[:data][:integrantes]
+				row << year[:data][:usuarios_mudaron]
+				row << year[:data][:catidad_mudanza]
+				row << year[:data][:mudados_siguen_participando]
+				row << year[:data][:mudados_siguen_participando_donde]
+				row << year[:data][:base_grupa]
+				row << year[:data][:otras_zonas]
+			end
+			csv << row
+		end
+
+		return csv_file
+	end
+end
