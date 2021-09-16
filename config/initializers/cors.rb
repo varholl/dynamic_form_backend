5c451df8698287b489d4585a93d81cc5612288e1
varholl <matias.laino@gmail.com>
2021-09-15 23:11:00 -0300

    Test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com', '127.0.0.1:5500'
+    origins 'formulario-socorro-rosa.netlify.app', '127.0.0.1:5500'
 
     resource '*',
       headers: :any,
